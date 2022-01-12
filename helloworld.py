1d46c7131cffdf2448dd66f9a2bd6ea6c375326e
Paul Cristerna <olivierpaulcristerna@gmail.com>
2022-01-12 14:58:29 -0700

    Se agregan tipos de datos en Python.

---
+++ helloworld.py
@@ -0,0 +1,56 @@
+# Strings
+
+print("Hello World")
+print('Hello World')
+print('''Hello World''')
+print("""Hello World""")
+
+print("Bye" + "World")
+
+# Integer
+
+print(30)
+
+# Float
+
+print(30.5)
+
+# Boolean
+
+False
+True
+
+# List
+
+[10, 20, 30, 40]
+['Hello' , 'Bye', 'World']
+[10, 'Hello', True, 10.1]
+[]
+
+# Tuples
+
+(10, 20, 30, 40)
+()
+
+# Json
+
+print(type({
+    "title": "Headline 1",
+    "desc": "Description 1",
+    "version": "1.0",
+}))
+
+json = [{
+    "title": "Headline 1",
+    "desc": "Description 1",
+    "version": "1.0",
+},
+{
+    "title": "Headline 2",
+    "desc": "Description 2",
+    "version": "1.0",
+}]
+
+print(json)
+
+
